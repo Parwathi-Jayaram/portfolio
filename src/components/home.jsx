d1add428321e9ae491f359c0e23075360a05e2c9
Parwathi-Jayaram <parwathijayaram1@gmail.com>
2025-11-12 13:31:56 +0530

    path

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,18 +1,24 @@
 import React from 'react';
+import bg from '../assets/bg.jpg';
+import calm from '../assets/calm.png';
+import angry from '../assets/angry.png';
 
 const Home = () => {
   return (
-    <div className="bg-[url('src/assets/bg.jpg')] bg-cover bg-center h-120 flex items-center justify-center">
+    <div
+      className="bg-cover bg-center h-120 flex items-center justify-center"
+      style={{ backgroundImage: `url(${bg})` }}
+    >
       <div className="relative w-120 h-120 group">
         <img
           id="mob-calm"
-          src="src/assets/calm.png"
+          src={calm}
           alt="Calm Mob"
           className="absolute w-150 h-120 transition-opacity duration-1500 ease-in-out opacity-100 group-hover:opacity-0"
         />
         <img
           id="mob-angry"
-          src="src/assets/angry.png"
+          src={angry}
           alt="Angry Mob"
           className="absolute w-150 h-120 transition-opacity duration-1500 ease-in-out opacity-0 group-hover:opacity-100"
         />
